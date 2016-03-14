8a576f00439695f2267f8a5771a285215d5f8cfd
Gabor Szarnyas <szarnyasg@gmail.com>
2016-03-14 15:37:32 +0100

    Refactoring

--- hu.bme.mit.yakindu.codegen/src/hu/bme/mit/yakindu/codegen/YakinduCodeGeneratorApplication.java
+++ hu.bme.mit.yakindu.codegen/src/hu/bme/mit/yakindu/codegen/YakinduCodeGeneratorApplication.java
@@ -1,33 +1,22 @@
 package hu.bme.mit.yakindu.codegen;
 
-import java.io.IOException;
-
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.yakindu.base.expressions.ExpressionsStandaloneSetup;
-import org.yakindu.sct.generator.core.GeneratorExecutor;
 import org.yakindu.sct.generator.genmodel.SGenStandaloneSetup;
-import org.yakindu.sct.model.sgen.GeneratorModel;
-import org.yakindu.sct.model.sgraph.Statechart;
 import org.yakindu.sct.model.stext.STextStandaloneSetup;
 
 public class YakinduCodeGeneratorApplication implements IApplication {
@@ -45,42 +34,12 @@ public class YakinduCodeGeneratorApplication implements IApplication {
 			final IProject project = setupProject();
 			// generateCodeFromSGenFile(project);
 
-			loadSgen();
+			YakinduUtil yakinduUtil = new YakinduUtil(STATECHART_BUNDLE_NAME, SGEN_RELATIVE_PATH, SCT_RELATIVE_PATH);
+			yakinduUtil.loadSgen();
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
 		return IApplication.EXIT_OK;
-	}
-
-	private void loadSgen() {
-//		IPath path = new Path(SGEN_RELATIVE_PATH);
-		Resource sgenResource = loadResource(getWorkspaceFileFor(SGEN_RELATIVE_PATH));
-		GeneratorModel model = (GeneratorModel) sgenResource.getContents().get(0);
-		model.getEntries().get(0).setElementRef(getStatechart());
-		new GeneratorExecutor().executeGenerator(model);
-	}
-
-	protected IFile getWorkspaceFileFor(final String path) {
-		return ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(STATECHART_BUNDLE_NAME + "/" + path));
-	}
-
-	protected Statechart getStatechart() {
-		IPath path = getWorkspaceFileFor(SCT_RELATIVE_PATH).getFullPath();
-		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
-		Resource resource = loadResource(file);
-		return (Statechart) resource.getContents().get(0);
-	}
-
-//	protected IPath getTargetPath() {
-//		return new Path(
-//				"/home/szarnyasg/runtime-hu.bme.mit.yakindu.codegen.codegen/hu.bme.mit.yakindu.codegen.callhandling");
-//	}
-
-	protected Resource loadResource(IFile file) {
-		Resource resource = null;
-		URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
-		resource = new ResourceSetImpl().getResource(uri, true);
-		return resource;
 	}
 
 	private IProject setupProject() throws CoreException, Exception {
@@ -129,22 +88,22 @@ public class YakinduCodeGeneratorApplication implements IApplication {
 		return workspaceRoot.getProject(STATECHART_BUNDLE_NAME);
 	}
 
-	private void generateCodeFromSGenFile(final IProject project) throws Exception {
-		final GeneratorExecutor generatorExecutor = new GeneratorExecutor();
-
-		final IFile sgenFile = project.getFile(SGEN_RELATIVE_PATH);
-		final boolean sgenFileExists = sgenFile.exists();
-		if (!sgenFileExists) {
-			throw new IOException("File " + SGEN_RELATIVE_PATH + " does not exist!");
-		}
-
-		ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, null);
-
-		System.out.println("Generating code from file: " + sgenFile.getRawLocationURI());
-		generatorExecutor.executeGenerator(sgenFile);
-		System.out.println("Code generation finished.");
-
-		ResourcesPlugin.getWorkspace().save(true, null);
-	}
+//	private void generateCodeFromSGenFile(final IProject project) throws Exception {
+//		final GeneratorExecutor generatorExecutor = new GeneratorExecutor();
+//
+//		final IFile sgenFile = project.getFile(SGEN_RELATIVE_PATH);
+//		final boolean sgenFileExists = sgenFile.exists();
+//		if (!sgenFileExists) {
+//			throw new IOException("File " + SGEN_RELATIVE_PATH + " does not exist!");
+//		}
+//
+//		ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, null);
+//
+//		System.out.println("Generating code from file: " + sgenFile.getRawLocationURI());
+//		generatorExecutor.executeGenerator(sgenFile);
+//		System.out.println("Code generation finished.");
+//
+//		ResourcesPlugin.getWorkspace().save(true, null);
+//	}
 
 }
---
+++ hu.bme.mit.yakindu.codegen/src/hu/bme/mit/yakindu/codegen/YakinduUtil.java
@@ -0,0 +1,52 @@
+package hu.bme.mit.yakindu.codegen;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.yakindu.sct.generator.core.GeneratorExecutor;
+import org.yakindu.sct.model.sgen.GeneratorModel;
+import org.yakindu.sct.model.sgraph.Statechart;
+
+public class YakinduUtil {
+
+	
+	private String statechartBundleName;
+	private String sgenRelativePath;
+	private String sctRelativePath;
+
+	public YakinduUtil(String statechartBundleName, String sgenRelativePath, String sctRelativePath) {
+		this.statechartBundleName = statechartBundleName;
+		this.sgenRelativePath = sgenRelativePath;
+		this.sctRelativePath = sctRelativePath;
+	}
+
+	public void loadSgen() {
+		Resource sgenResource = loadResource(getWorkspaceFileFor(sgenRelativePath));
+		GeneratorModel model = (GeneratorModel) sgenResource.getContents().get(0);
+		model.getEntries().get(0).setElementRef(getStatechart(sctRelativePath));
+		new GeneratorExecutor().executeGenerator(model);
+	}
+
+	public Statechart getStatechart(final String sctLocation) {
+		IPath path = getWorkspaceFileFor(sctLocation).getFullPath();
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+		Resource resource = loadResource(file);
+		return (Statechart) resource.getContents().get(0);
+	}
+	
+	protected Resource loadResource(IFile file) {
+		Resource resource = null;
+		URI uri = URI.createPlatformResourceURI(file.getFullPath().toString(), true);
+		resource = new ResourceSetImpl().getResource(uri, true);
+		return resource;
+	}
+	
+	protected IFile getWorkspaceFileFor(final String path) {
+		return ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(statechartBundleName + "/" + path));
+	}
+
+}
